94cb5dd6ab3000ce77e4ba3a2d59dcf0f55ba860
Logan Tegman <ltegman@gmail.com>
2016-01-11 10:29:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-beginner-book
+
+Just a repo of my code after following along with the [node beginner book](http://www.nodebeginner.org/). No original code here I can take credit for.
---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+
+function start(route, handle) {
+	function onRequest(req, res) {
+		let postData = '';
+		const pathname = url.parse(req.url).pathname;
+		console.log('Request for ' + pathname + ' recieved.');
+		route(handle, pathname, res, req);
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log('Server has started.');
+}
+
+exports.start = start;
